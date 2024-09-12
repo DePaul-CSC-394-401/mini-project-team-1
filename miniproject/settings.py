48cfed7daac1177c76f0a8e59cb1cf55c0acf4f9
Diego Izaguirre <tenochca.di@outlook.com>
2024-09-12 00:20:21 -0500

    created dockerfile, requirements, docker-compose, set up env for password

--- miniproject/settings.py
+++ miniproject/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'miniproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todo_db',
+        'USER': 'postgres',
+        'PASSWORD': 'percy18dI!',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
