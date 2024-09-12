21f44ad85bdccaa42f5ed163300a16db74149c9b
Diego Izaguirre <tenochca.di@outlook.com>
2024-09-12 13:23:02 -0500

    created db model for TASKS and made db migrations

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    task_name = models.CharField(max_length=200)
+    task_description = models.TextField(null=True, blank=True)
+    completed = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.task_name
+    
