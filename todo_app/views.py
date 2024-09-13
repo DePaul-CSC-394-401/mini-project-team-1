ca53314026dc0ef3b5d98745d8883c145aa6dfde
brian wu <witenout@gmail.com>
2024-09-13 14:51:23 -0500

    Login_and_registeration

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,4 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 def index(request):
     return render(request, 'index.html')
+
+
+def signup(request):
+    
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password= request.POST.get('password')
+        passowrdcont= request.POST.get('passwordcont')
+        email= request.POST.get('email')
+        
+        myuser = User.objects.create(username, password, email)
+        myuser.save()
+        return redirect('signin')
+    return render(request, 'signup.html')
+
+def userlogin(request):
+    return render(request, 'userlogin.html')
+
+def signin(request):
+    if request.method =='POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return render(request, 'signin.html')
+        else:
+            return redirect('index')
+            
+    return render(request, 'userlogin.html')
+
+def logout(request):
+    pass
