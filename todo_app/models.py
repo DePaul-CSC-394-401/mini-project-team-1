16864091a1e5a10c9fcc45d83206464b45f780e7
jtcastillojr <jtcastillojr@gmail.com>
2024-09-13 21:58:03 -0500

    Added in To-Do List Page
    Created a To-Do List HTML page and still working on getting things displayed

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Task(models.Model):
     task_name = models.CharField(max_length=200)
--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from django.contrib import admin
+from .views import taskList
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('signup', views.signup, name='signup'),
+    path('userlogin', views.userlogin, name='userlogin'),
+    path('logout', views.logout, name='logout'),
+    path('tasks', views.taskList, name='tasks')
 ]
 
