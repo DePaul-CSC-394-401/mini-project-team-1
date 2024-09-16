6f0c8f7d4792b94794becb3ced3751e58355094d
mansurisaad555 <mansurisaad555@gmail.com>
2024-09-16 15:06:51 -0500

    Added containers to align username and profile management and remove completed checkbox from create task section

--- todo_app/forms.py
+++ todo_app/forms.py
@@ -8,7 +8,7 @@ class TaskForm(forms.ModelForm):
 
     class Meta:
         model = Task
-        fields = ['task_name', 'task_description', 'completed', 'priority']
+        fields = ['task_name', 'task_description', 'priority']
 
 # ** Email Update Form for Profile Management **
 class EmailUpdateForm(forms.ModelForm):
